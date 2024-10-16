da2d45f79b8655b3ed5ce4eb3392b2f9ce45a6ff
Marcin Bojara <marcin_5@mail.ru>
2024-10-16 14:46:12 +0200

    Change route and parameter mapping in WeatherController
    Updated the route for the highlanderSaysApi endpoint and switched from a route parameter to a query parameter with a default value of 5. This makes the API more flexible and simplifies usage by allowing the threshold to be specified in the query string.

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -7,17 +7,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[Route('/weather')]
 class WeatherController extends AbstractController
 {
-    #[Route('/highlander-says/{threshold<\d+>?5}', host: 'api.localhost')]
-    public function highlanderSaysApi(int $threshold): Response
+    #[Route('/highlander-says/api')]
+    public function highlanderSaysApi(#[MapQueryParameter] int $threshold = 5): Response
     {
         $draw = random_int(0, 10);
         $json = ['forecast' => $draw < $threshold ? "It's going to rain" : "It's going to be sunny",
+            'threshold' => $threshold,
             'self' => $this->generateUrl('app_weather_highlandersaysapi',
                 ['threshold' => $threshold],
                 UrlGeneratorInterface::ABSOLUTE_URL
