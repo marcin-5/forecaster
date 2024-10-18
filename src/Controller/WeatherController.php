68117d52e03e9e72fe0e5f81c50cc64d17e2031a
Marcin Bojara <marcin_5@mail.ru>
2024-10-18 20:49:19 +0200

    Add weather forecast template and mock data
    Implemented a weather forecast page using Twig templating and embedded mock forecast data within the WeatherController. The HTML structure now includes sections for temperature, feels-like temperature, pressure, humidity, wind speed, cloudiness, and icon. This lays out the forecast for three days for a specified city and country code.

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -1,19 +1,61 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/weather')]
+
 class WeatherController extends AbstractController
 {
-    #[Route('/{countryCode}/{city}')]
+    #[Route("/weather/{countryCode}/{city}")]
     public function forecast(string $countryCode, string $city): Response
     {
-        $content = "<html><body>Forecast for $countryCode: $city</body></html>";
-        return new Response($content);
+        $forecasts = [
+            [
+                "date" => new \DateTime('2024-01-01'),
+                "temperatureCelsius" => 17,
+                "flTemperatureCelsius" => 16,
+                "pressure" => 1000,
+                "humidity" => 64,
+                "wind_speed" => 3.2,
+                "wind_deg" => 270,
+                "cloudiness" => 75,
+                'icon' => 'sun',
+            ],
+            [
+                "date" => new \DateTime('2024-01-02'),
+                "temperatureCelsius" => 12,
+                "flTemperatureCelsius" => 13,
+                "pressure" => 1001,
+                "humidity" => 62,
+                "wind_speed" => 2.2,
+                "wind_deg" => 180,
+                "cloudiness" => 50,
+                'icon' => 'cloud',
+            ],
+            [
+                "date" => new \DateTime('2024-01-03'),
+                "temperatureCelsius" => 11,
+                "flTemperatureCelsius" => 12,
+                "pressure" => 1010,
+                "humidity" => 63,
+                "wind_speed" => 3.5,
+                "wind_deg" => 270,
+                "cloudiness" => 15,
+                'icon' => 'cloud-rain',
+            ],
+        ];
+
+        $response = $this->render('weather/forecast.html.twig', [
+            'forecasts' => $forecasts,
+            'city' => $city,
+            'countryCode' => $countryCode,
+        ]);
+
+        return $response;
     }
 }
