9bf0afd506cfeee27393fb51c178eaa42acea21e
Marcin Bojara <marcin_5@mail.ru>
2024-10-26 15:13:04 +0200

    Enhance LocationForm with specific field types and constraints
    Updated the LocationForm to use TextType, ChoiceType, and NumberType with defined constraints for 'name', 'countryCode', 'latitude', and 'longitude' fields. This ensures better validation and user input handling in the form.

--- src/Controller/LocationFormController.php
+++ src/Controller/LocationFormController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Location;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,13 +17,37 @@ class LocationFormController extends AbstractController
     public function new(): Response
     {
         $location = new Location();
-        $location->setCountryCode('PL');
-
         $form = $this->createFormBuilder($location)
-            ->add('name')
-            ->add('countryCode')
-            ->add('latitude')
-            ->add('longitude')
+            ->add('name', TextType::class)
+            ->add('countryCode', ChoiceType::class, [
+                'choices' => [
+                    '' => null,
+                    'Poland' => 'PL',
+                    'Germany' => 'DE',
+                    'France' => 'FR',
+                    'India' => 'IN',
+                    'United Kingdom' => 'UK',
+                    'United States' => 'US',
+                ]
+            ])
+            ->add('latitude', NumberType::class, [
+                'html5' => true,
+                'scale' => 7,
+                'attr' => [
+                    'step' => 0.1,
+                    'min' => -90,
+                    'max' => 90,
+                ]
+            ])
+            ->add('longitude', NumberType::class, [
+                'html5' => true,
+                'scale' => 7,
+                'attr' => [
+                    'step' => 0.1,
+                    'min' => -180,
+                    'max' => 180,
+                ]
+            ])
             ->getForm();
 
         return $this->render('location_form/new.html.twig', [
