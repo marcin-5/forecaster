6f0cf0802bfa8385d71cf5ec14b3f178d210fd6d
Marcin Bojara <marcin_5@mail.ru>
2024-11-02 19:45:39 +0100

    Enable CSV output for HighlanderSayCommand
    Added a new `csv` option to the HighlanderSayCommand to render weather forecasts as CSV. Updated the Twig template to include a threshold value in the CSV output.

--- src/Command/HighlanderSayCommand.php
+++ src/Command/HighlanderSayCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Twig\Environment;
 
 #[AsCommand(
     name: 'highlander:say',
@@ -17,7 +18,8 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class HighlanderSayCommand extends Command
 {
     public function __construct(
-        private Highlander $highlander,
+        private Highlander  $highlander,
+        private Environment $twig,
     )
     {
         parent::__construct();
@@ -27,7 +29,8 @@ class HighlanderSayCommand extends Command
     {
         $this
             ->addOption('threshold', 't', InputOption::VALUE_REQUIRED, 'The threshold for specifying the weather', 50)
-            ->addOption('trials', 'r', InputOption::VALUE_REQUIRED, 'Count of forecasts to provide', 1);
+            ->addOption('trials', 'r', InputOption::VALUE_REQUIRED, 'Count of forecasts to provide', 1)
+            ->addOption('csv', 'c', InputOption::VALUE_NONE, 'Render in CSV');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -36,10 +39,19 @@ class HighlanderSayCommand extends Command
 
         $threshold = $input->getOption('threshold');
         $trials = $input->getOption('trials');
+        $isCsv = $input->getOption('csv');
 
         $forecasts = $this->highlander->generateWeatherForecasts($threshold, $trials);
 
-        $io->listing($forecasts);
+        if ($isCsv) {
+            $csv = $this->twig->render('weather/highlander_says.csv.twig', [
+                'forecasts' => $forecasts,
+                'threshold' => $threshold,
+            ]);
+            $io->writeln($csv);
+        } else {
+            $io->listing($forecasts);
+        }
 
         return Command::SUCCESS;
     }
