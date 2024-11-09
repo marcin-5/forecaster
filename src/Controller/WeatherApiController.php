0080e5cf749a0d5dda2adf45fc76426ac7659006
Marcin Bojara <marcin_5@mail.ru>
2024-11-09 13:23:54 +0100

    Add Location form handling to WeatherApiController
    Implemented a new LocationApiType form and integrated it into the WeatherApiController to handle location data submission. This allows for the validation and persistence of new Location entities via a POST request to the /location-form endpoint.

--- src/Controller/WeatherApiController.php
+++ src/Controller/WeatherApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Location;
+use App\Form\LocationApiType;
 use App\Repository\ForecastRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -141,5 +142,31 @@ class WeatherApiController extends AbstractController
             'success' => true,
             'location' => $location,
         ]);
+    }
+
+    #[Route('/location-form')]
+    public function postLocationForm(
+        Request                $request,
+        EntityManagerInterface $em,
+    ): JsonResponse
+    {
+        $location = new Location();
+        $form = $this->createForm(LocationApiType::class, $location);
+        $form->submit($request->request->all());
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($location);
+            $em->flush();
+
+            return $this->json([
+                'success' => true,
+                'location' => $location,
+            ]);
+        } else {
+            return $this->json([
+                'success' => false,
+                'errors' => (string)$form->getErrors()
+            ], Response::HTTP_BAD_REQUEST);
+        }
     }
 }
---
+++ src/Form/LocationApiType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Location;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocationApiType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('countryCode')
+            ->add('latitude')
+            ->add('longitude')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+}
